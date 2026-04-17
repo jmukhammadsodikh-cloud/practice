adc8086c9273b1b1c9067f97873ab86e6e2c8584
MARCO <jmukhammadsodikh@gmail.com>
2026-04-17 18:30:47 +0900

    feat: train.js

---
+++ train.js
@@ -0,0 +1,17 @@
+/*
+A-TASK ========= (NodeJS)
+Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni ikkinchi parametrdagi sozdan qatnashga sonini return qilishi kerak boladi.
+MASALAN countLetter("e", "engineer") 3ni return qiladi.
+*/
+// Masalani yechimi loop for bilan qlamiz
+
+function countLetter(letter, word) {
+    let count = 0;
+    for (let index = 0; index < word.length; index++)
+        if (word[index] === letter) {
+            count++
+        }
+    return count;
+}
+const result = countLetter("a", "assalomu aleykum");
+console.log("result:", result); 
