ef602f1876dfa27a70217b5bd8d8ca59f7bbde8b
MARCO <jmukhammadsodikh@gmail.com>
2026-04-19 18:12:04 +0900

    feat: specific error handling system

--- objects.py
+++ objects.py
@@ -27,3 +27,23 @@ print("result:", result)
 # from package import ceil = aniq bir method chaqirmoqchi bolsak
 result2 = ceil(33.9)
 print("result2:", result2)
+
+
+print("=========Error handling sysytem==============")
+# Hatolikning oldini olish
+
+car_obj = dict(name="FERRARI", year=2024, sport_car=True)
+try:
+    print(f"passed here")
+    result = car_obj["sport_car"]
+    print(f"result:", result)
+    a = car_obj.speed()
+except KeyError as err:
+    print(f"No origin state property found", err)
+except AttributeError as err:
+    print(f"No spped method found", err)
+else:
+    print(f"Executed successfully without errors")
+
+finally:
+    print(f"Final closing logic")
