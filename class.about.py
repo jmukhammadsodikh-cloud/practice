ffbdb72fab3952d0ea794be49b6971075ddbfff9
MARCO <jmukhammadsodikh@gmail.com>
2026-04-20 16:05:49 +0900

    fix: python special/magic methods

--- class.about.py
+++ class.about.py
@@ -1,7 +1,7 @@
 '''CLASS
 (1) What is class
 (2) Ordinary vs static properties
-(3) Special methods
+(3) Special methods / magic methods
 '''
 print("=======WHAT IS CLASS=============")
 # class = blueprint for object creation!
@@ -58,3 +58,46 @@ print(f"new_message:", new_message)
 #  def explain(cls):
 # print(f"static method property executed")
 Person.explain()
+
+print("=======Special methods / magic methods=============")
+# Python's special most common special magic methods are below:
+# __init__ __new__ __str__ __call__ __getitem__ __eq__ __item__ ....
+
+
+class Car():
+    # state
+    descripion = 'This class makes cars'
+    # constructor
+
+    def __new__(cls, *args):
+        print("*__new__*")
+        return super().__new__(cls)
+
+    def __init__(self, name, year):
+        self.name = name
+        self.year = year
+
+    # method
+
+    def start_engine(self):
+        print(f"the {self.name} started engine!!")
+
+    def stop_engine(self):
+        print(f"the {self.name} stopped engine!!")
+
+    def __str__(self):
+        return f"{self.name} was produced in {self.year} year!"
+
+    def __call__(self):
+        print("Object called as function!")
+        return True
+
+
+my_car = Car("Ferrari", 2025)
+my_car.start_engine()
+my_car.stop_engine()
+print("------")
+your_car = Car("Toyota", 2023)
+print(your_car)
+response = your_car()  # Call as a function
+print("response:", response)
