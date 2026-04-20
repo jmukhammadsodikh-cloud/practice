cf758eaaf2de8e6abebb6df44a7c33d541dadd8e
MARCO <jmukhammadsodikh@gmail.com>
2026-04-20 14:15:39 +0900

    feat: learn class and its ordinary and static property

---
+++ class.about.py
@@ -0,0 +1,60 @@
+'''CLASS
+(1) What is class
+(2) Ordinary vs static properties
+(3) Special methods
+'''
+print("=======WHAT IS CLASS=============")
+# class = blueprint for object creation!
+# structure = state constructor method
+
+
+class Person():
+    # state
+    message = "class state property"
+    # constuctor = magic methods orqali hosil qilamiz
+
+    def __init__(self, name, age, occupation):
+        self.name = name
+        self.age = age
+        self.occupation = occupation
+
+    # method
+    def introduce(self):
+        print(f"This man {self.name} says Hey how do you do!")
+
+    def say_age(self):
+        print(f"{self.name} says I am {self.age} !")
+
+    def say_occupation(self):
+        print(f"I am a {self.occupation}")
+
+    @classmethod
+    def explain(cls):
+        print(f"static method property executed")
+
+
+person1 = Person("MARCO", 22, "Web developer")
+person2 = Person("SAM", 29, "Policeman")
+person3 = Person("MBAPPE", 30, "Football player")
+
+# ordinary state =  oddiy object bilan yashaydi
+print("person1.name:", person1.name)
+
+# ordinary method = oddiy object bilan yashaydi
+person1.introduce()
+person2.say_age()
+person3.say_occupation()
+
+print("=======Ordinary vs Static properties=============")
+# Ordinary = oddiy object bilan yashaydi
+# Static properties = Class bilan yashaydi
+
+# static state = class bilan chaqiriladi
+new_message = Person.message
+print(f"new_message:", new_message)
+
+# CLASS DECORATOR orqali static method hosil qilishimiz mumkun
+# @classmethod
+#  def explain(cls):
+# print(f"static method property executed")
+Person.explain()
