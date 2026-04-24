2e35f01638e978a1f12f330cc22b7cc5f25c03dd
MARCO <jmukhammadsodikh@gmail.com>
2026-04-24 17:39:47 +0900

    feat: unpacking arguments via tuple

--- tuple.py
+++ tuple.py
@@ -3,7 +3,7 @@
     (2) Unpacking arguments
     (3) Zip
 """
-print("======")
+print("=========================================== What is tuple ================================================")
 # Java/PHP/NodeJS array => Pythonda esa array = list
 
 # literal
@@ -29,3 +29,25 @@ tuple_obj = ("MARCO", 23, True, None)
 
 print(animals[1])
 # animals[1] = "pig"   # we cannot mutate tuple
+
+print("=========================================== Unpacking arguments ================================================")
+# argumentlarni yoyish = Tuple bilan
+groups = ("MIT", "DEVEX", "FLEXY", "MG")
+(a, b, *c) = groups
+print(f"the:a {a} and the d: {c}")
+
+# Qoida = () qavis qoyish kerak
+
+# args > tuple = qachonki argumentlarimiz soni noaniq bolsa bitta qlib ovolamiz
+
+
+def calculate(*args):
+    print("args >", args)
+    total = 1
+    for x in args:
+        total += x
+    print(f"the total value: {total}")
+    return total
+
+
+calculate(10, 20)
