b6f04791c6f4c9508fcd935dee98b59f47dd39c3
MARCO <jmukhammadsodikh@gmail.com>
2026-04-27 17:14:22 +0900

    fix: specific operators with set

--- array.set.py
+++ array.set.py
@@ -18,3 +18,33 @@ print("numbers(3):", numbers)
 
 del numbers[0:2]
 print("numbers(4):", numbers)
+
+print("========== Set ==========")
+# {set} of unique collection without keeping order !
+# qaytarilmaydigan unique toplam
+new_numbers = array("i", [1, 4, 7, 5, 4, 7, 8, 9, 9])
+numbs_set = set(new_numbers)
+
+print("numbs_set:", numbs_set)
+
+numbs_set.add(200)
+print("numbs_set(1)", numbs_set)
+
+numbs_set.add(7)
+print("numbs_set(2)", numbs_set)
+
+
+print("========== Specific operators with set ==========")
+# | & - ^
+a = {10, 20, 50}
+b = {20, 40}
+
+result1 = a | b     # union => bu ikkala toplamni bitta set qlib beradi
+result2 = a & b    # intersection => ikkita setda bir hil bolgan toplamni beradi
+result3 = a - b   # difference => ikkita setni ayrib beradi
+result4 = a ^ b  # symetric difference => ikkita setda bir hil bolmagan toplamni beradigit
+
+print("result1:", result1)
+print("result2:", result2)
+print("result3:", result3)
+print("result4:", result4)
